7a6dd770a18d44d84423f833dfae31a694a8b054
sanmiguelmario <42873283+sanmiguelmario@users.noreply.github.com>
2019-02-07 09:12:20 -0600

    Update reto_1.js

--- reto_1.js
+++ reto_1.js
@@ -4,9 +4,9 @@ function setup() {
 
 function draw() {
   background(0);
-	fill(255,0,0)
-	stroke(255)
-	line(200,0,200,200)
-	line(0,400,200,200)
-	line(200,200,400,400)
+	fill(255,0,0);
+	stroke(255);
+	line(200,0,200,200);
+	line(0,400,200,200);
+	line(200,200,400,400);
 }
