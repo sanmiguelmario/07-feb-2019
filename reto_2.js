e1f1439c8a5db067b47bda3c798056d9adb16c3e
sanmiguelmario <42873283+sanmiguelmario@users.noreply.github.com>
2019-02-07 09:25:24 -0600

    Create reto_2.js

---
+++ reto_2.js
@@ -0,0 +1,16 @@
+function setup() {
+  createCanvas(400, 400);
+}
+
+function draw() {
+  background(255);
+	rect(0,0,133,400);
+	fill(0,255,0);
+rect(133,0,133,400);
+	fill(0,0,255);
+	rect(266,0,133,400);
+	fill(255,0,0);
+	
+	
+}
+
