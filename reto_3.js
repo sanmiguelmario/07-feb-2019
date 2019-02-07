87abab8a4c14bf16fd0a677c8a6090cca2970e0b
sanmiguelmario <42873283+sanmiguelmario@users.noreply.github.com>
2019-02-07 09:17:35 -0600

    Create reto_3.js

---
+++ reto_3.js
@@ -0,0 +1,13 @@
+function setup() {
+  createCanvas(400, 400);
+}
+
+function draw() {
+  background(255);
+	fill(255);
+	stroke(255,0,0);
+	strokeWeight(20);
+	ellipse(200,200,255,255);
+	line(110,110,280,280);
+}
+
